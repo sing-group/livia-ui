4ae89b762718037acacdab09df2ba37ab0206ad2
fctato <fctato@esei.uvigo.es>
2021-03-16 20:18:35 +0100

    Adds live analyzer configuration saving and new saving mode
    Adds live analyzer configuration save and load to the same XML file used
    previously for the shortcuts configuration. It will be saved with each change
    to the analyzer and loaded on the application startup.
    Also now the file writing is safer. It's saved to a temporal ".xml.new" file,
    then the older file is overwritten whit the new version, and then the temporal
    file is removed.

---
+++ livia_ui/gui/configuration/AnalyzerConfigurationStorage.py
@@ -0,0 +1,55 @@
+from ast import literal_eval
+from xml.etree.ElementTree import Element, SubElement, ParseError
+
+from livia.process.analyzer.FrameAnalyzerManager import FrameAnalyzerManager
+from livia.process.analyzer.NoChangeFrameAnalyzer import NoChangeFrameAnalyzer
+from livia_ui.gui import LIVIA_GUI_LOGGER
+from livia_ui.gui.status.FrameProcessingStatus import FrameProcessingStatus
+
+
+class AnalyzerConfigurationStorage:
+    def __init__(self, processing_status: FrameProcessingStatus):
+        self._processing_status: FrameProcessingStatus = processing_status
+
+    def save_configuration(self) -> Element:
+        actual_analyzer = self._processing_status.live_frame_analyzer
+        root = Element("analyzers")
+        live_configuration_write = SubElement(root, "live-analyzer")
+        live_configuration_write.set("class", str(actual_analyzer.__class__.__name__))
+
+        metadata = FrameAnalyzerManager.get_metadata_for(actual_analyzer)
+
+        for prop in metadata.properties:
+            value = getattr(actual_analyzer, prop.name)
+            if not prop.hidden and prop.default_value != value:
+                property_write = SubElement(live_configuration_write, "property")
+                property_write.set("name", prop.name)
+                property_write.text = str(value)
+        return root
+
+    def read_configuration(self, configuration_root: Element):
+        try:
+            live_analyzer_root = configuration_root.find("live-analyzer")
+
+            live_analyzer = NoChangeFrameAnalyzer()
+            live_analyzer_properties = None
+
+            for analyzer in FrameAnalyzerManager.list_analyzers():
+                if analyzer.analyzer_class.__name__ == live_analyzer_root.get("class"):
+                    live_analyzer = analyzer.analyzer_class()
+                    live_analyzer_properties = analyzer.properties
+                    break
+
+            for prop_readed in live_analyzer_root:
+                for prop in live_analyzer_properties:
+                    if prop.hidden:
+                        continue
+
+                    if prop_readed.get("name") == prop.name:
+                        setattr(live_analyzer, prop.name, literal_eval(prop_readed.text))
+                        break
+
+            self._processing_status.live_frame_analyzer = live_analyzer
+
+        except ParseError:
+            LIVIA_GUI_LOGGER.exception("Error parsing analyzer configuration file")
--- livia_ui/gui/configuration/ConfigurationStorage.py
+++ livia_ui/gui/configuration/ConfigurationStorage.py
@@ -1,11 +1,15 @@
+import os
+import shutil
 from typing import Optional
 from xml.etree import ElementTree
 from xml.etree.ElementTree import Element
 
 from livia.process.listener import build_listener
 from livia_ui.gui import LIVIA_GUI_LOGGER
-from livia_ui.gui.configuration.ShortcutsConfiguration import ShortcutsConfiguration
+from livia_ui.gui.configuration.AnalyzerConfigurationStorage import AnalyzerConfigurationStorage
+from livia_ui.gui.configuration.ShortcutsConfigurationStorage import ShortcutsConfigurationStorage
 from livia_ui.gui.status.LiviaStatus import LiviaStatus
+from livia_ui.gui.status.listener.FrameProcessingStatusChangeListener import FrameProcessingStatusChangeListener
 from livia_ui.gui.status.listener.ShortcutStatusChangeEvent import ShortcutStatusChangeEvent
 from livia_ui.gui.status.listener.ShortcutStatusChangeListener import ShortcutStatusChangeListener
 
@@ -17,9 +21,12 @@ class ConfigurationStorage:
 
         self._configuration_root: Element = None
         self._shortcuts_configuration_root: Element = None
+        self._analyzer_configuration_root: Element = None
 
-        self._shortcuts_configuration: ShortcutsConfiguration = \
-            ShortcutsConfiguration(self._livia_status.shortcut_status)
+        self._shortcuts_configuration: ShortcutsConfigurationStorage = \
+            ShortcutsConfigurationStorage(self._livia_status.shortcut_status)
+        self._analyzer_configuration: AnalyzerConfigurationStorage = \
+            AnalyzerConfigurationStorage(self._livia_status.video_stream_status)
 
         self._read_configuration()
 
@@ -33,14 +40,21 @@ class ConfigurationStorage:
                            shortcut_removed=self._save_configuration
                            )
         )
+        self._livia_status.video_stream_status.add_frame_processing_status_change_listener(
+            build_listener(FrameProcessingStatusChangeListener,
+                           live_frame_analyzer_changed=self._save_configuration)
+        )
 
     def _read_configuration(self):
         try:
             self._configuration_root = ElementTree.parse(self._file_path).getroot()
             self._shortcuts_configuration_root = self._configuration_root.find("shortcuts")
+            self._analyzer_configuration_root = self._configuration_root.find("analyzers")
 
             if self._shortcuts_configuration_root is not None:
                 self._shortcuts_configuration.read_configuration(self._shortcuts_configuration_root)
+            if self._analyzer_configuration_root is not None:
+                self._analyzer_configuration.read_configuration(self._analyzer_configuration_root)
         except FileNotFoundError:
             LIVIA_GUI_LOGGER.exception("Configuration file '" + self._file_path + "' not found")
         except ElementTree.ParseError:
@@ -50,10 +64,16 @@ class ConfigurationStorage:
         configuration = ElementTree.Element("configuration")
 
         shortcuts = self._shortcuts_configuration.save_configuration()
+        analyzer = self._analyzer_configuration.save_configuration()
 
         configuration.append(shortcuts)
+        configuration.append(analyzer)
 
         datastr = ElementTree.tostring(configuration)
-        file = open(self._file_path, "wb")
+        file = open(self._file_path + '.new', "wb")
         file.write(datastr)
         file.close()
+
+        shutil.copy(self._file_path + '.new', self._file_path)
+
+        os.remove(self._file_path + '.new')
