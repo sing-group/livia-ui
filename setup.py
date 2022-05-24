f74a3d01f2ca963048cb63b416050a8967b317a3
Alejandro González García <aggarcia3@esei.uvigo.es>
2022-05-24 10:22:17 +0200

    Adds log format and level analyzer parameters to the CLI
    This builds upon the foundation established in commit
    ed95b7f4642a4b76a906bfd8a6a2bd4854ff05d3. These parameters are
    straightforward facades for the Python logging system.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="livia-ui",
-    version="0.1.dev59",
+    version="0.1.dev60",
     author="Fernando Campos Tato, Miguel Reboiro-Jato, Daniel Glez-Peña, Florentino Fdez-Riverola, Rubén Domínguez Carbajales, Hugo López-Fdez, Alba Nogueira-Rodríguez",
     author_email="fctato@esei.uvigo.es, mrjato@uvigo.es, dgpena@uvigo.es, riverola@uvigo.es, RUBEN.DOMINGUEZ.CARBAJALES@sergas.es, hlfernandez@uvigo.es, alnogueira@uvigo.es",
     description="User interfaces for the LIVIA framework.",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
-        "livia-core==0.1.dev74",
+        "livia-core==0.1.dev75",
         "PySide2==5.15.2"
     ],
     extras_require={
